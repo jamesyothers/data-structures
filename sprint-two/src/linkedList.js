9c816c14df2a77af9de23a2c884e8ec7f10f70c0
Hack Reactor Students <james.yothers@gmail.com>
2014-06-05 20:37:40 -0700

    Sprint 1 complete / Sprint 2 set complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    if (this.head === this.tail && this.head !== null) {
+      var copy = this.head;
+      delete this.head;
+      this.head = null;
+      this.tail = null;
+    } else if(this.head !== this.tail) {
+      var copy = this.head;
+      delete this.head;
+      this.head = copy.next;
+    }
+    return copy.value;
   };
 
   list.contains = function(target){
+    var index = this.head;
+    while (index !== null) {
+      if (index.value === target) {
+        return true;
+      } else {
+        index = index.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +55,10 @@ var makeNode = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var list = makeLinkedList();
+
+list.addToTail(4);
+if(list.head() === list.tail()){
+}
+
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,26 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage === undefined){
+    this._storage = {};
+  }
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage !== undefined){
+    return item in this._storage;
+  }
+  return false;
+
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
+  if(Object.keys(this._storage).length === 0){
+    this._storage = undefined;
+  }
+
 };
 
 /*
